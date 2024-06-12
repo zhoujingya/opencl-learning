bca0e8246116c8a9e75cbce49a3b0c73c694cdd0
zhoujing <jing.zhou@terapines.com>
2024-06-12 20:39:09 +0800

    Add util to get device information

--- utils/utils.h
+++ utils/utils.h
@@ -1,13 +1,27 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#include <CL/opencl.hpp>
 #include <string>
 #include <vector>
 
 namespace utils {
+
+std::string err_code(cl_int err_in);
+
+void check_error(cl_int err, std::string operation, std::string filename,
+                 int line);
+
+#define checkError(E, S) check_error(E, S, __FILE__, __LINE__)
+
 void readFile(const char *Filename, std::string &Content);
 
 void generateRandomVec(std::vector<float> &Vec, int Size);
+
+std::vector<cl::Platform> getPlatForms();
+
+std::vector<cl::Device> getPlatFormDevice(cl::Platform &);
+
 } // namespace utils
 
 #endif // _UTILS_H_
