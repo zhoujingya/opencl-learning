766fdb7771c5438a07e9ff4a20e22d4ab7afe52b
zhoujing <jing.zhou@terapines.com>
2024-06-13 17:21:37 +0800

    Minor change

--- src/vecadd/vecadd.cl
+++ src/vecadd/vecadd.cl
@@ -1,5 +1,9 @@
 kernel void vecadd(global float *a, global float *b, global float *c)
 {
-    int i = get_global_id(0);
+    int i = get_global_id(0) +(get_global_id(1) * get_global_size(0)) + (get_global_id(2) * get_global_size(0) * get_global_size(1));
+
+    // printf("x id: %d\n",i);
+
     c[i] = a[i] + b[i];
+
 };
--- src/vecadd/vecadd.cpp
+++ src/vecadd/vecadd.cpp
@@ -7,6 +7,8 @@
 
 using namespace utils;
 
+// cl::Kernel()
+
 int main() {
   using namespace std;
   // const char *KernelFile = VECADD_SOURCE;
@@ -33,28 +35,28 @@ int main() {
   } else {
     cout << "Build Success" << endl;
   }
-  cl::Buffer BufferA(Context, CL_MEM_READ_WRITE, sizeof(float) * 1024);
-  cl::Buffer BufferB(Context, CL_MEM_READ_WRITE, sizeof(float) * 1024);
-  cl::Buffer BufferC(Context, CL_MEM_READ_WRITE, sizeof(float) * 1024);
+  cl::Buffer BufferA(Context, CL_MEM_READ_WRITE, sizeof(float) * 2048);
+  cl::Buffer BufferB(Context, CL_MEM_READ_WRITE, sizeof(float) * 2048);
+  cl::Buffer BufferC(Context, CL_MEM_READ_WRITE, sizeof(float) * 2048);
 
   cl::CommandQueue Queue(Context, Device);
   std::vector<float> A;
   std::vector<float> B;
-  generateRandomVec(A, 1024);
-  generateRandomVec(B, 1024);
-  Queue.enqueueWriteBuffer(BufferA, true, 0, sizeof(float) * 1024, &A[0]);
-  Queue.enqueueWriteBuffer(BufferB, true, 0, sizeof(float) * 1024, &B[0]);
+  generateRandomVec(A, 2048);
+  generateRandomVec(B, 2048);
+  Queue.enqueueWriteBuffer(BufferA, true, 0, sizeof(float) * 2048, &A[0]);
+  Queue.enqueueWriteBuffer(BufferB, true, 0, sizeof(float) * 2048, &B[0]);
   cl::Kernel Kernel(Program, "vecadd");
   Kernel.setArg(0, BufferA);
   Kernel.setArg(1, BufferB);
   Kernel.setArg(2, BufferC);
-  Queue.enqueueNDRangeKernel(Kernel, cl::NullRange, cl::NDRange(1024));
+  Queue.enqueueNDRangeKernel(Kernel, cl::NullRange, cl::NDRange(16, 64, 2));
   Queue.finish();
-  std::vector<float> C(1024);
-  Queue.enqueueReadBuffer(BufferC, true, 0, sizeof(float) * 1024, &C[0]);
+  std::vector<float> C(2048);
+  Queue.enqueueReadBuffer(BufferC, true, 0, sizeof(float) * 2048, &C[0]);
 
   // test
-  for (int i = 0; i < 1024; i++) {
+  for (int i = 0; i < 2048; i++) {
     if (A[i] + B[i] != C[i]) {
       cout << A[i] << ": " << B[i] << ": " << C[i] << "error" << i << endl;
       exit(1);
